a24d63990fef6c02bbd6b66596ea3ecbfd866d97
davidleacock <dave.leacock@gmail.com>
2024-08-26 13:11:29 -0700

    add remaining endpoints

--- src/main/scala/http/UserServer.scala
+++ src/main/scala/http/UserServer.scala
@@ -10,9 +10,28 @@ object UserServer {
 
   private def routes: Routes[UserRepository, Response] =
     Routes(
-      Method.GET / "test" -> handler(Response.text("test worked.")),
-
-      // TODO: Test this endpoint
+      Method.GET / "users" / string("id") -> handler { (id: String, _: Request) =>
+        ZIO
+          .serviceWithZIO[UserRepository](_.get(id))
+          .mapBoth(
+            err => Response.internalServerError(s"Problem ${err.getMessage}"),
+            {
+              case Some(user) => Response.text(s"User: $user")
+              case None       => Response.notFound(s"User $id not found.")
+            }
+          )
+      },
+      Method.GET / "users" -> handler { (_: Request) =>
+        ZIO
+          .serviceWithZIO[UserRepository](_.list())
+          .mapBoth(
+            err => Response.internalServerError(s"Problem ${err.getMessage}"),
+            users => {
+              val response = users.map(user => s"ID: ${user.id} Name: ${user.name}").mkString("\n")
+              Response.text(response)
+            }
+          )
+      },
       Method.POST / "users" -> handler { (req: Request) =>
         for {
           user <- req.body.to[User].orElseFail(Response.badRequest)
@@ -23,7 +42,8 @@ object UserServer {
               user => Response.text(user.toString)
             )
         } yield response
-      }
+      },
+      Method.GET / "test" -> handler(Response.text("HTTP Server online..."))
     )
 
   val server: ZIO[UserRepository with Server, Throwable, Nothing] =
