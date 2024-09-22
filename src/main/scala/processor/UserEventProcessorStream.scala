8213c4e1ccdd21ac5d6951b8b0250b0a7a19d809
davidleacock <dave.leacock@gmail.com>
2024-09-22 14:28:46 -0700

    Refactor the UserEventProcessor so that it returns a ZStream rather than ZIO, also change the return type to not be Unit

---
+++ src/main/scala/processor/UserEventProcessorStream.scala
@@ -0,0 +1,32 @@
+package processor
+
+import consumer.IngressUserAccountConsumer
+import domain.UserAccount
+import repo.UserAccountRepository
+import zio._
+import zio.stream.ZStream
+
+case class EventProcessorSuccess(userAccount: UserAccount)
+case class EventProcessorFail(reason: String)
+
+object UserEventProcessorStream {
+
+  def processStream
+    : ZStream[IngressUserAccountConsumer with UserAccountRepository, EventProcessorFail, EventProcessorSuccess] =
+    ZStream.unwrap {
+      for {
+        consumer <- ZIO.service[IngressUserAccountConsumer]
+        repo <- ZIO.service[UserAccountRepository]
+      } yield consumer
+        .consume
+        .mapError(err => EventProcessorFail(s"Error processing from ingress stream: ${err.getMessage}"))
+        .mapZIO { user =>
+          repo
+            .create(user)
+            .mapBoth(
+              err => EventProcessorFail(s"Error persisting to user account repo: ${err.getMessage}"),
+              _ => EventProcessorSuccess(user)
+            )
+        }
+    }
+}
